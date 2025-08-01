4d84f628855f62bd5faa5a93acd0c723e16b9381
Balaji Venkatraman <rslbalaji@gmail.com>
2025-08-01 16:28:09 +0530

    add mv

--- app/pipelines/three_d_generation/__init__.py
+++ app/pipelines/three_d_generation/__init__.py
@@ -2,5 +2,6 @@ from .pipeline import Hunyuan3DGenerationPipeline
 from .components.preprocessor import ViewPreprocessor3D
 from .components.shape_generator import ShapeGenerator3D    
 from .components.texture_generator import TextureGenerator3D
+from .components.rembg import BackgroundRemover
 
-__all__ = ["Hunyuan3DGenerationPipeline", "ViewPreprocessor3D", "ShapeGenerator3D", "TextureGenerator3D"]
+__all__ = ["Hunyuan3DGenerationPipeline", "ViewPreprocessor3D", "ShapeGenerator3D", "TextureGenerator3D", "BackgroundRemover"]
--- app/pipelines/three_d_generation/pipeline.py
+++ app/pipelines/three_d_generation/pipeline.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from PIL import Image
 from .components.shapegen import Hunyuan3DDiTFlowMatchingPipeline
 from .components.texgen import Hunyuan3DPaintPipeline
+from .components.rembg import BackgroundRemover
 
 class Hunyuan3DGenerationPipeline:
     def __init__(self):
@@ -9,10 +10,37 @@ class Hunyuan3DGenerationPipeline:
         self.texture_gen = Hunyuan3DPaintPipeline()
     
     def generate(self):
-        pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained('tencent/Hunyuan3D-2')
-        mesh = pipeline(image='../../inputs/left.png')[0]
+        images = {
+            "front": "app/inputs/front.png",
+            "left": "app/inputs/left.png",
+            "back": "app/inputs/back.png",
+            "right": "app/inputs/right.png",
+        }
 
-        pipeline = Hunyuan3DPaintPipeline.from_pretrained('tencent/Hunyuan3D-2')
-        mesh = pipeline(mesh, image='../../inputs/left.png')
-        print("Generated mesh")
+        for key in images:
+            image = Image.open(images[key]).convert("RGBA")
+            if image.mode == 'RGB':
+                rembg = BackgroundRemover()
+                image = rembg(image)
+                images[key] = image
+        # Shape generation
+        pipeline = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(
+            'tencent/Hunyuan3D-2mv',
+            subfolder='hunyuan3d-dit-v2-mv',
+            variant='fp16'
+        )
+        mesh = pipeline(
+            image=images,
+            num_inference_steps=50,
+            octree_resolution=380,
+            num_chunks=20000,
+            generator=torch.manual_seed(12345),
+            output_type='trimesh'
+        )[0]
+        mesh.export(f'mesh.glb')
+
+        # Texture generation
+        # pipeline = Hunyuan3DPaintPipeline.from_pretrained('tencent/Hunyuan3D-2')
+        # mesh = pipeline(mesh, image='../../inputs/left.png')
+        # print("Generated mesh")
 
